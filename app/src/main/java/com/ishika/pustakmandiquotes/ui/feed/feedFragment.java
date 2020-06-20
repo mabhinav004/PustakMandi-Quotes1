a864df1054f2c1643e260378694dd8caf0c98cc9
Ishika-png <ishika.agg2000@gmail.com>
2020-06-20 19:18:58 +0530

    commentScreen

--- app/src/main/java/com/ishika/pustakmandiquotes/ui/feed/feedFragment.java
+++ app/src/main/java/com/ishika/pustakmandiquotes/ui/feed/feedFragment.java
@@ -1,9 +1,11 @@
 package com.ishika.pustakmandiquotes.ui.feed;
 
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -12,6 +14,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.ishika.pustakmandiquotes.R;
+
 
 import java.util.ArrayList;
 
@@ -22,23 +25,25 @@ public class feedFragment extends Fragment {
     feedAdapter adapter;
     ArrayList<String> items;
 
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         feedViewModel =
                 ViewModelProviders.of(this).get(feedViewModel.class);
         View root = inflater.inflate(R.layout.feedfrag, container, false);
-        items=new ArrayList<>();
+        items = new ArrayList<>();
         items.add("Mishra topper");
         items.add("Mishra pro topper");
         items.add("Mishra ultimate topper");
         items.add("Mishra ultimate pro topper");
         items.add("Mishra infinity topper max");
-        recyclerView= root.findViewById(R.id.FeedRecycle);
+        recyclerView = root.findViewById(R.id.FeedRecycle);
         final LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
         layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
         recyclerView.setLayoutManager(layoutManager);
-        adapter=new feedAdapter(getContext(),items);
+        adapter = new feedAdapter(getContext(), items);
         recyclerView.setAdapter((RecyclerView.Adapter) adapter);
         return root;
+
     }
 }
